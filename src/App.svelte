964cbe4e54b6165010b7511928d9d850973ad80a
ItaloSSilva19 <iti.s.silva@gmail.com>
2021-08-13 11:41:10 -0300

    configuração inicial

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<h1 class="text-center"> Lista Telefônica </h1>
+<div class="
+position-absolute top-50 start-50 translate-middle
+shadow rounded bg-light bg-gradient text-dark 
+p-3 mb-2 w-50 h-75 "
+>
+	
+	
+</div>
+
